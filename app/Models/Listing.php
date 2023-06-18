8e51a917c6ffa3a65a9aed8573c41b91caffe818
farid guzman <farid.guzman91@hotmail.com>
2023-06-18 09:58:51 +0000

    update models and new  instances

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Listing extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['beds', 'baths', 'area', 'city', 'street', 'street_nr', 'code', 'price'];
 }
+
+/*       Listing::create(['beds' => 2, 'baths' => 2, 'area' => 100, 'city' => 'North', 'street' => 'Tinker st', 'street_nr' => 20, 'code' => 'TS', 'price' => 200_000]) */
