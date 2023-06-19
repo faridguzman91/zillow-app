239d01487dcab6b5d246b1b855e1cb58d84b94a4
farid guzman <farid.guzman91@hotmail.com>
2023-06-19 13:20:00 +0000

    added styles, new tailwind pckg, inertial progress indicator

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,25 +1,37 @@
 <template>
-    <Link href="/listing">Listings</Link>&nbsp;
+    <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full">
+        <Link href="/listing/create">New Listing</Link>
+        <div class="container mx-auto">
+            <nav class="p-4 flex items-center justify-between">
+                <div v-if="flashSuccess" class="success">
+                    <div class="text-lg font-medium">
+                        {{ flashSuccess }}
+                        <Link :href="route('listing.index')">Listings</Link>
+                    </div>
+                </div>
+                <slot>Default</slot>
+                <div class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center">
+                    <Link :href="route('listing.index')">zillow-app</Link>
+                </div>
+                <div>
+                    <Link :href="route('listing.create')" class="btn-primary">+ New Listing</Link>
+                </div>
+            </nav>
+        </div>
+    </header>
 
-    <Link href="/listing/create">Create Listing</Link>
-
-    <!-- <div> -->
-    <!--     timer = {{ timer }} -->
-    <!-- </div> -->
-
-    <div v-if="flashSuccess" class="success">
-        {{ flashSuccess }}
-
-    </div>
-
-    <slot>Default</slot>
-
-
+    <main class="container mx-auto p-4">
+        <div v-if="flashSuccess"
+            class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+            {{ flashSuccess }}
+        </div>
+        <slot>Default</slot>
+    </main>
 </template>
 
 <script setup>
 
-import { Link , usePage } from '@inertiajs/vue3'
+import { Link, usePage } from '@inertiajs/vue3'
 import { computed } from 'vue'
 
 //page.props.value.flash.success
@@ -27,14 +39,5 @@ const page = usePage()
 const flashSuccess = computed(
     () => page.props.flash.success,
 )
-
-
 </script>
-
-<style scoped>
-.success {
-    background-color: green;
-    color: white;
-}
-</style>
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,30 @@
-    import { createApp, h } from 'vue'
-    import { createInertiaApp } from '@inertiajs/vue3'
+import { createInertiaApp } from "@inertiajs/vue3";
+import { createApp, h } from "vue";
 /*     import { createInertiaApp } from '@inertiajs/inertia-vue3'; */
-    import MainLayout from './Layouts/MainLayout.vue'
+import { InertiaProgress } from "@inertiajs/progress";
+import { ZiggyVue } from "ziggy";
+import "../css/app.css";
+import MainLayout from "./Layouts/MainLayout.vue";
 
+InertiaProgress.init({
+    delay: 0,
+    color: "#29d",
+    includeCSS: true,
+    showSpinner: true,
+});
 
-    createInertiaApp({
-      resolve: name => {
-        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
 
-        let page = pages[`./Pages/${name}.vue`]
-        page.default.layout = page.default.layout || MainLayout
-        return page
-      },
-      setup({ el, App, props, plugin }) {
+        let page = pages[`./Pages/${name}.vue`];
+        page.default.layout = page.default.layout || MainLayout;
+        return page;
+    },
+    setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
-          .use(plugin)
-          .mount(el)
-      },
-    })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+});
